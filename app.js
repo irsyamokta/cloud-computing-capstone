c1434c2c7c84061fc420d46d3a2284cf4db9fdb7
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-11-14 22:55:41 +0700

    Fix : Port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const auth = require('./routes/auth.js');
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
