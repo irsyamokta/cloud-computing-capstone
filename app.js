a9af43f5f049d98ae2e6eec132c1dbf68bde5f7c
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-11-15 00:44:31 +0700

    Test : CI/CD Cloud Build

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Welcome Polafit Webservice");
 });
 
 app.use('/auth', auth);
